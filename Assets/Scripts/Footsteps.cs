47f8e967bd35f651ca0e83c3a2838bf768df19da
Weronika Wypchło <shepkashepkax@gmail.com>
2026-02-03 18:39:59 +0100

    koniec

--- Assets/Scripts/Footsteps.cs
+++ Assets/Scripts/Footsteps.cs
@@ -164,7 +164,7 @@ public class Footsteps : MonoBehaviour
             soundInstance = RuntimeManager.CreateInstance(eventRef);
             soundInstance.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject.transform));
             // Ustawia parametr FMOD na podstawie ustalonej wartości.
-            soundInstance.setParameterByNameWithLabel("Footsteps", surfaceParameter); 
+            soundInstance.setParameterByNameWithLabel("Surface", surfaceParameter); 
             soundInstance.start();
             soundInstance.release();
         }
